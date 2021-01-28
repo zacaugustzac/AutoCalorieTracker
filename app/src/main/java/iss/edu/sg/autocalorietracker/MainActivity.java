ca3e3798a9e8a4f17ad41e67b4eb3d842b69609b
lkaditya <69144603+lkaditya@users.noreply.github.com>
2021-01-28 14:59:34 +0800

    add the necessary part to communicate with the camera and the webapp server

--- app/src/main/java/iss/edu/sg/autocalorietracker/MainActivity.java
+++ app/src/main/java/iss/edu/sg/autocalorietracker/MainActivity.java
@@ -2,13 +2,28 @@ package iss.edu.sg.autocalorietracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button move;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        move=findViewById(R.id.scan);
+        move.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==move){
+            Intent in= new Intent(this,CaptureActivity.class);
+            startActivity(in);
+        }
     }
 }
