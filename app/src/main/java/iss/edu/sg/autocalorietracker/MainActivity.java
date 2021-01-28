25ae25dc096c87ca35796fe7eda9247916214f80
Nicole Xinyue <huanxinyue21@gmail.com>
2021-01-28 18:01:30 +0800

    Update the diagram and menu bar

--- app/src/main/java/iss/edu/sg/autocalorietracker/MainActivity.java
+++ app/src/main/java/iss/edu/sg/autocalorietracker/MainActivity.java
@@ -1,14 +1,133 @@
 package iss.edu.sg.autocalorietracker;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private BarChart barChart;
+    private YAxis leftAxis;
+    private YAxis rightAxis;
+    private boolean hasAxesNames = true;
+    private LimitLine ll1;
+
+    //variables for menu
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //hooks
+        drawerLayout =findViewById(R.id.drawer_layout);
+        navigationView =findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        //tool bar
+        setSupportActionBar(toolbar);
+
+        //Navigation Drawer Menu
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+
+
+        barChart = findViewById(R.id.barChart);
+
+        //set x value
+        String[] days = new String[]{"", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(days));
+
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setGranularity(1);
+        xAxis.setGranularityEnabled(true);
+
+        //set value of each column
+        BarDataSet barDataSet = new BarDataSet(calories(), "Calories");
+        barDataSet.setColors(getResources().getColor(R.color.green));
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setDrawValues(false);
+
+
+        BarData barData = new BarData(barDataSet);
+
+        barChart.setDragEnabled(true);
+        barChart.setFitBars(true);
+        barChart.setData(barData);
+        barChart.animateY(1000);
+        barChart.setDrawBorders(false);
+
+        Description description = new Description();
+        description.setEnabled(false);
+        barChart.setDescription(description);
+
+        leftAxis = barChart.getAxisLeft();
+        rightAxis = barChart.getAxisRight();
+
+        xAxis.setDrawAxisLine(false);
+        rightAxis.setEnabled(false);
+        barChart.setDrawGridBackground(false);
+        xAxis.setDrawGridLines(false);
+        leftAxis.setDrawGridLines(false);
+
+
+    }
+
+
+    @Override
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    private ArrayList<BarEntry> calories() {
+        ArrayList<BarEntry> calories = new ArrayList<>();
+        calories.add(new BarEntry(1, 450));
+        calories.add(new BarEntry(2, 650));
+        calories.add(new BarEntry(3, 560));
+        calories.add(new BarEntry(4, 400));
+        calories.add(new BarEntry(5, 750));
+        calories.add(new BarEntry(6, 650));
+        calories.add(new BarEntry(7, 450));
+        return calories;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
     }
 }
