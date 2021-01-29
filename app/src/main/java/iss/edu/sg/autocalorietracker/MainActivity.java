e0f5bc2afe7508ebe3e4af2682fbc1b5873aecc8
zacaugustzac <zacaugustzac@gmail.com>
2021-01-30 04:54:15 +0800

    updated to latest gradle

--- app/src/main/java/iss/edu/sg/autocalorietracker/MainActivity.java
+++ app/src/main/java/iss/edu/sg/autocalorietracker/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.graphics.Color;
@@ -33,160 +34,303 @@ import com.google.android.material.navigation.NavigationView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
-    private BarChart barChart;
-    private YAxis leftAxis;
-    private YAxis rightAxis;
-    private boolean hasAxesNames = true;
-    private LimitLine ll1;
-    private ImageView menuIcon;
-    private ImageView photoIcon;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.view.LineChartView;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
-    //variables for menu
-    private DrawerLayout drawerLayout;
-    private NavigationView navigationView;
-    private Toolbar toolbar;
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.listener.ComboLineColumnChartOnValueSelectListener;
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.Column;
+import lecho.lib.hellocharts.model.ColumnChartData;
+import lecho.lib.hellocharts.model.ComboLineColumnChartData;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.SubcolumnValue;
+import lecho.lib.hellocharts.util.ChartUtils;
+import lecho.lib.hellocharts.view.ComboLineColumnChartView;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //hooks
-        drawerLayout =findViewById(R.id.drawer_layout);
-        navigationView =findViewById(R.id.nav_view);
-        menuIcon = findViewById(R.id.menu);
-        photoIcon = findViewById(R.id.photo);
-        photoIcon.setOnClickListener(this);
-
-
-        //tool bar
-        setSupportActionBar(toolbar);
-
-        //Navigation Drawer Menu
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-
-        navigationDrawer();
-
-
-        barChart = findViewById(R.id.barChart);
-
-        //set x value
-        String[] days = new String[]{"", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-        XAxis xAxis = barChart.getXAxis();
-        xAxis.setValueFormatter(new IndexAxisValueFormatter(days));
-
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setGranularity(1);
-        xAxis.setGranularityEnabled(true);
-
-        //set value of each column
-        BarDataSet barDataSet = new BarDataSet(calories(), "Calories");
-        barDataSet.setColors(getResources().getColor(R.color.green));
-        barDataSet.setValueTextColor(Color.BLACK);
-        barDataSet.setDrawValues(false);
-
-
-        BarData barData = new BarData(barDataSet);
-
-        barChart.setDragEnabled(true);
-        barChart.setFitBars(true);
-        barChart.setData(barData);
-        barChart.animateY(1000);
-        barChart.setDrawBorders(false);
-
-        Description description = new Description();
-        description.setEnabled(false);
-        barChart.setDescription(description);
-
-        leftAxis = barChart.getAxisLeft();
-        rightAxis = barChart.getAxisRight();
-
-        rightAxis.setEnabled(false);
-        barChart.setDrawGridBackground(false);
-        xAxis.setDrawGridLines(false);
-        leftAxis.setDrawGridLines(false);
-
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
+        }
 
     }
 
-    private void navigationDrawer() {
-        //Navigation Drawer
-        navigationView.bringToFront();
-        navigationView.setNavigationItemSelectedListener(this);
-        navigationView.setCheckedItem(R.id.nav_home);
+    /**
+     * A fragment containing a combo line/column chart view.
+     */
+    public static class PlaceholderFragment extends Fragment {
 
-        menuIcon.setOnClickListener(new View.OnClickListener(){
+        private ComboLineColumnChartView chart;
+        private ComboLineColumnChartData data;
 
-            @Override
-            public void onClick(View v) {
-                if(drawerLayout.isDrawerVisible(GravityCompat.START)){
-                    drawerLayout.closeDrawer(GravityCompat.START);
-                }else {
-                    drawerLayout.openDrawer(GravityCompat.START);
+        private int numberOfLines = 1;
+        private int maxNumberOfLines = 4;
+        private int numberOfPoints = 12;
+
+        float[][] randomNumbersTab = new float[maxNumberOfLines][numberOfPoints];
+
+        private boolean hasAxes = true;
+        private boolean hasAxesNames = true;
+        private boolean hasPoints = true;
+        private boolean hasLines = true;
+        private boolean isCubic = false;
+        private boolean hasLabels = false;
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            setHasOptionsMenu(true);
+            View rootView = inflater.inflate(R.layout.fragment_combo_line_column_chart, container, false);
+
+            chart = (ComboLineColumnChartView) rootView.findViewById(R.id.chart);
+            chart.setOnValueTouchListener(new ValueTouchListener());
+
+            generateValues();
+            generateData();
+
+            return rootView;
+        }
+
+        // MENU
+//        @Override
+//        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+//            inflater.inflate(R.menu.combo_line_column_chart, menu);
+//        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+            if (id == R.id.action_reset) {
+                reset();
+                generateData();
+                return true;
+            }
+            if (id == R.id.action_add_line) {
+                addLineToData();
+                return true;
+            }
+            if (id == R.id.action_toggle_lines) {
+                toggleLines();
+                return true;
+            }
+            if (id == R.id.action_toggle_points) {
+                togglePoints();
+                return true;
+            }
+            if (id == R.id.action_toggle_cubic) {
+                toggleCubic();
+                return true;
+            }
+            if (id == R.id.action_toggle_labels) {
+                toggleLabels();
+                return true;
+            }
+            if (id == R.id.action_toggle_axes) {
+                toggleAxes();
+                return true;
+            }
+            if (id == R.id.action_toggle_axes_names) {
+                toggleAxesNames();
+                return true;
+            }
+            if (id == R.id.action_animate) {
+                prepareDataAnimation();
+                chart.startDataAnimation();
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+
+        private void generateValues() {
+            for (int i = 0; i < maxNumberOfLines; ++i) {
+                for (int j = 0; j < numberOfPoints; ++j) {
+                    randomNumbersTab[i][j] = (float) Math.random() * 50f + 5;
                 }
             }
-        });
-    }
+        }
 
-    //set the drawer
-    @Override
-    public void onBackPressed(){
-        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
-            drawerLayout.closeDrawer(GravityCompat.START);
-        }else{
-            super.onBackPressed();
+        private void reset() {
+            numberOfLines = 1;
+
+            hasAxes = true;
+            hasAxesNames = true;
+            hasLines = true;
+            hasPoints = true;
+            hasLabels = false;
+            isCubic = false;
+
+        }
+
+        private void generateData() {
+            // Chart looks the best when line data and column data have similar maximum viewports.
+            data = new ComboLineColumnChartData(generateColumnData(), generateLineData());
+
+            if (hasAxes) {
+                Axis axisX = new Axis();
+                Axis axisY = new Axis().setHasLines(true);
+                if (hasAxesNames) {
+                    axisX.setName("Axis X");
+                    axisY.setName("Axis Y");
+                }
+                data.setAxisXBottom(axisX);
+                data.setAxisYLeft(axisY);
+            } else {
+                data.setAxisXBottom(null);
+                data.setAxisYLeft(null);
+            }
+
+            chart.setComboLineColumnChartData(data);
+        }
+
+        private LineChartData generateLineData() {
+
+            List<Line> lines = new ArrayList<Line>();
+            for (int i = 0; i < numberOfLines; ++i) {
+
+                List<PointValue> values = new ArrayList<PointValue>();
+                for (int j = 0; j < numberOfPoints; ++j) {
+                    values.add(new PointValue(j, randomNumbersTab[i][j]));
+                }
+
+                Line line = new Line(values);
+                line.setColor(ChartUtils.COLORS[i]);
+                line.setCubic(isCubic);
+                line.setHasLabels(hasLabels);
+                line.setHasLines(hasLines);
+                line.setHasPoints(hasPoints);
+                lines.add(line);
+            }
+
+            LineChartData lineChartData = new LineChartData(lines);
+
+            return lineChartData;
+
+        }
+
+        private ColumnChartData generateColumnData() {
+            int numSubcolumns = 1;
+            int numColumns = 12;
+            // Column can have many subcolumns, here by default I use 1 subcolumn in each of 8 columns.
+            List<Column> columns = new ArrayList<Column>();
+            List<SubcolumnValue> values;
+            for (int i = 0; i < numColumns; ++i) {
+
+                values = new ArrayList<SubcolumnValue>();
+                for (int j = 0; j < numSubcolumns; ++j) {
+                    values.add(new SubcolumnValue((float) Math.random() * 50 + 5, ChartUtils.COLOR_GREEN));
+                }
+
+                columns.add(new Column(values));
+            }
+
+            ColumnChartData columnChartData = new ColumnChartData(columns);
+            return columnChartData;
+        }
+
+        private void addLineToData() {
+            if (data.getLineChartData().getLines().size() >= maxNumberOfLines) {
+                Toast.makeText(getActivity(), "Samples app uses max 4 lines!", Toast.LENGTH_SHORT).show();
+                return;
+            } else {
+                ++numberOfLines;
+            }
+
+            generateData();
+        }
+
+        private void toggleLines() {
+            hasLines = !hasLines;
+
+            generateData();
+        }
+
+        private void togglePoints() {
+            hasPoints = !hasPoints;
+
+            generateData();
+        }
+
+        private void toggleCubic() {
+            isCubic = !isCubic;
+
+            generateData();
+        }
+
+        private void toggleLabels() {
+            hasLabels = !hasLabels;
+
+            generateData();
+        }
+
+        private void toggleAxes() {
+            hasAxes = !hasAxes;
+
+            generateData();
+        }
+
+        private void toggleAxesNames() {
+            hasAxesNames = !hasAxesNames;
+
+            generateData();
+        }
+
+        private void prepareDataAnimation() {
+
+            // Line animations
+            for (Line line : data.getLineChartData().getLines()) {
+                for (PointValue value : line.getValues()) {
+                    // Here I modify target only for Y values but it is OK to modify X targets as well.
+                    value.setTarget(value.getX(), (float) Math.random() * 50 + 5);
+                }
+            }
+
+            // Columns animations
+            for (Column column : data.getColumnChartData().getColumns()) {
+                for (SubcolumnValue value : column.getValues()) {
+                    value.setTarget((float) Math.random() * 50 + 5);
+                }
+            }
+        }
+
+        private class ValueTouchListener implements ComboLineColumnChartOnValueSelectListener {
+
+            @Override
+            public void onValueDeselected() {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onColumnValueSelected(int columnIndex, int subcolumnIndex, SubcolumnValue value) {
+                Toast.makeText(getActivity(), "Selected column: " + value, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPointValueSelected(int lineIndex, int pointIndex, PointValue value) {
+                Toast.makeText(getActivity(), "Selected line point: " + value, Toast.LENGTH_SHORT).show();
+            }
+
         }
     }
-
-    //set the item in menu bar
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem){
-        switch (menuItem.getItemId()){
-            case R.id.nav_home:
-                break;
-            case R.id.nav_profile:
-                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.nav_plan:
-                Intent intent1 = new Intent(MainActivity.this, PlanActivity.class);
-                startActivity(intent1);
-                break;
-            case R.id.nav_reminders:
-                Intent intent2 = new Intent(MainActivity.this, RemindersActivity.class);
-                startActivity(intent2);
-                break;
-            case R.id.nav_share:
-                Toast.makeText(this,"Share",Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.nav_rate:
-                break;
-        }
-        drawerLayout.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-
-    @Override
-    public void onClick(View v) {
-            Intent intent= new Intent(this,CaptureActivity.class);
-            startActivity(intent);
-
-    }
-    private ArrayList<BarEntry> calories() {
-        ArrayList<BarEntry> calories = new ArrayList<>();
-        calories.add(new BarEntry(1, 450));
-        calories.add(new BarEntry(2, 650));
-        calories.add(new BarEntry(3, 560));
-        calories.add(new BarEntry(4, 400));
-        calories.add(new BarEntry(5, 750));
-        calories.add(new BarEntry(6, 650));
-        calories.add(new BarEntry(7, 450));
-        return calories;
-    }
-
 }
