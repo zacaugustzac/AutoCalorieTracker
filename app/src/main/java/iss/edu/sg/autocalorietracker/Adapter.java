31d9063b6bc7e9ff8b1abd90e87515554d18b954
Nicole Xinyue <huanxinyue21@gmail.com>
2021-01-30 18:07:05 +0800

    Update the Adapter

--- app/src/main/java/iss/edu/sg/autocalorietracker/Adapter.java
+++ app/src/main/java/iss/edu/sg/autocalorietracker/Adapter.java
@@ -1,6 +1,7 @@
 package iss.edu.sg.autocalorietracker;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,68 +9,177 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+    private ArrayList<Item> mItemList;
 
-    List<Integer> images;
-    List<String> name;
-    List<String> calorie;
-    List<String> timestamp;
-    LayoutInflater inflater;
+//    List<Integer> images;
+//    List<String> name;
+//    List<String> calorie;
+//    List<String> timestamp;
+//    LayoutInflater inflater;
 
-    public Adapter(List<Integer> images, List<String> name, List<String> calorie, List<String> timestamp, Context context) {
-        this.images = images;
-        this.name = name;
-        this.calorie = calorie;
-        this.timestamp = timestamp;
-        this.inflater = inflater.from(context);
+    private OnItemClickListener mListener;
+
+    public Adapter(ArrayList<Item> exampleList) {
+        mItemList = exampleList;
+    }
+    public interface OnItemClickListener {
+        void onShareClick(int position);
+        void onEditClick(int position);
+        void onDeleteClick(int position);
     }
 
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = inflater.inflate(R.layout.historyitem, parent, false);
-        return new ViewHolder(view);
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        mListener = listener;
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.name.setText(name.get(position));
-        holder.images.setImageResource(images.get(position));
-        holder.calorie.setText(calorie.get(position));
-        holder.timestamp.setText(timestamp.get(position));
-    }
+//    public Adapter(List<Integer> images, List<String> name, List<String> calorie, List<String> timestamp, Context context) {
+//        this.images = images;
+//        this.name = name;
+//        this.calorie = calorie;
+//        this.timestamp = timestamp;
+//        this.inflater = inflater.from(context);
+//    }
 
 
-    @Override
-    public long getItemId(int position) {
-        return 0;
-    }
-
-    @Override
-    public int getItemCount() {
-        return name.size();
-    }
 
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
         TextView name;
-        ImageView images;
+        ImageView image;
         TextView calorie;
         TextView timestamp;
+        ImageView share;
+        ImageView edit;
+        ImageView delete;
+
+        public ViewHolder(View itemView, final OnItemClickListener listener) {
+            super(itemView);
+            name = itemView.findViewById(R.id.catalogue);
+            image = itemView.findViewById(R.id.img);
+            calorie = itemView.findViewById(R.id.kcal);
+            timestamp = itemView.findViewById(R.id.timestamp);
+            share  = itemView.findViewById(R.id.share);
+            edit  = itemView.findViewById(R.id.edit);
+            delete  = itemView.findViewById(R.id.delete);
+            delete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onDeleteClick(position);
+                        }
+                    }
+                }
+            });
+        }
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            name = itemView.findViewById(R.id.catalogue);
-            images = itemView.findViewById(R.id.img);
-            calorie = itemView.findViewById(R.id.kcal);
-            timestamp = itemView.findViewById(R.id.timestamp);
         }
     }
 
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.historyitem, parent, false);
+        ViewHolder evh = new ViewHolder(v, mListener);
+        return evh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Item currentItem = mItemList.get(position);
+        holder.image.setImageResource(currentItem.getImage());
+        holder.name.setText(currentItem.getName());
+        holder.calorie.setText(currentItem.getCalorie());
+        holder.timestamp.setText(currentItem.getTimestamp());
+    }
+    @Override
+    public int getItemCount() {
+        return mItemList.size();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+//    @NonNull
+//    @Override
+//    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        View view = inflater.inflate(R.layout.historyitem, parent, false);
+//        ViewHolder vh = new ViewHolder(view, mListener);
+//        return vh;
+//    }
+
+
+
+//    @Override
+//    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+//        holder.name.setText(name.get(position));
+//        holder.images.setImageResource(images.get(position));
+//        holder.calorie.setText(calorie.get(position));
+//        holder.timestamp.setText(timestamp.get(position));
+//    }
+//
+//
+//    @Override
+//    public long getItemId(int position) {
+//        return 0;
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return name.size();
+//    }
+//
+//
+//    public class ViewHolder extends RecyclerView.ViewHolder {
+//        public View deleteExercise;
+//        public View edit_t;
+//        TextView name;
+//        ImageView images;
+//        TextView calorie;
+//        TextView timestamp;
+//        ImageView share;
+//        ImageView edit;
+//        ImageView delete;
+//
+//        public ViewHolder(@NonNull View itemView, OnItemClickListener listener) {
+//            super(itemView);
+//            name = itemView.findViewById(R.id.catalogue);
+//            images = itemView.findViewById(R.id.img);
+//            calorie = itemView.findViewById(R.id.kcal);
+//            timestamp = itemView.findViewById(R.id.timestamp);
+//            share  = itemView.findViewById(R.id.share);
+//            edit  = itemView.findViewById(R.id.edit);
+//            delete  = itemView.findViewById(R.id.delete);
+//
+//            delete.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    if (listener != null) {
+//                        int position = getAdapterPosition();
+//                        if (position != RecyclerView.NO_POSITION) {
+//                            listener.onDeleteClick(position);
+//                        }
+//                    }
+//                }
+//            });
+//        }
+//    }
+//}
---
+++ app/src/main/java/iss/edu/sg/autocalorietracker/Item.java
@@ -0,0 +1,50 @@
+package iss.edu.sg.autocalorietracker;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Item {
+
+    private Integer image;
+    private String name;
+    private String calorie;
+    private String timestamp;
+
+    public Item(Integer image, String name, String calorie, String timestamp) {
+        this.image = image;
+        this.name = name;
+        this.calorie = calorie;
+        this.timestamp = timestamp;
+    }
+
+
+
+    public Integer getImage() {
+        return image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCalorie() {
+        return calorie;
+    }
+
+    public void setCalorie(String calorie) {
+        this.calorie = calorie;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
