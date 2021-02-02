6fe7648dacd8f8e05821a10a9ac101f081df6bb4
Nicole Xinyue <huanxinyue21@gmail.com>
2021-02-02 13:21:58 +0800

    Update SignIn SignUp, FlashActivity

---
+++ app/src/main/java/iss/edu/sg/autocalorietracker/LoginActivity.java
@@ -0,0 +1,53 @@
+package iss.edu.sg.autocalorietracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button forgotpsw, signIn, signUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        forgotpsw = findViewById(R.id.forgotpsw);
+        signIn = findViewById(R.id.signIn);
+        signUp = findViewById(R.id.signUp);
+        setListeners();
+    }
+
+    private void setListeners() {
+        forgotpsw.setOnClickListener(this);
+        signIn.setOnClickListener(this);
+        signUp.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = null;
+        switch (view.getId()) {
+            case R.id.signIn:
+                intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.signUp:
+                intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.forgotpsw:
+                break;
+
+            default:
+                break;
+        }
+    }
+}
