bf0a171c1689d03464e8a9d8c6843b46d0959c31
Nicole Xinyue <huanxinyue21@gmail.com>
2021-02-03 09:23:39 +0800

    Update the radio part

--- app/src/main/java/iss/edu/sg/autocalorietracker/SignUpActivity.java
+++ app/src/main/java/iss/edu/sg/autocalorietracker/SignUpActivity.java
@@ -2,23 +2,27 @@ package iss.edu.sg.autocalorietracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 
-public class SignUpActivity extends AppCompatActivity{
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Spinner year;
     private ImageView weight1,weight2,weight3,height1,height2,height3;
     private TextView w1,w2,w3,h1,h2,h3;
     private RadioGroup radioGroup,radioWeight,radioHeight;
+    private Button save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +46,9 @@ public class SignUpActivity extends AppCompatActivity{
         height2 = findViewById(R.id.height2);
         height3 = findViewById(R.id.height3);
 
+        save = findViewById(R.id.save);
+        save.setOnClickListener(this);
+
         populateSpinnerYear();
         setRadio();
     }
@@ -58,15 +65,12 @@ public class SignUpActivity extends AppCompatActivity{
                         height1.setImageDrawable(getResources().getDrawable(R.drawable.male));
                         height2.setImageDrawable(getResources().getDrawable(R.drawable.male));
                         height3.setImageDrawable(getResources().getDrawable(R.drawable.male));
-                        w1.setText("1");
-                        w2.setText("1");
-                        w3.setText("1");
-                        h1.setText("1");
-                        w1.setText("1");
-                        w2.setText("1");
-                        w3.setText("1");
-
-
+                        w1.setText("50kg-60kg");
+                        w2.setText("61kg-70kg");
+                        w3.setText("71kg-80kg");
+                        h1.setText("160cm-170cm");
+                        h2.setText("171cm-180cm");
+                        h3.setText("181cm-190cm");
                         break;
                     case R.id.female:
                         weight1.setImageDrawable(getResources().getDrawable(R.drawable.f1));
@@ -75,13 +79,12 @@ public class SignUpActivity extends AppCompatActivity{
                         height1.setImageDrawable(getResources().getDrawable(R.drawable.female));
                         height2.setImageDrawable(getResources().getDrawable(R.drawable.female));
                         height3.setImageDrawable(getResources().getDrawable(R.drawable.female));
-                        w1.setText("1");
-                        w2.setText("1");
-                        w3.setText("1");
-                        h1.setText("1");
-                        w1.setText("1");
-                        w2.setText("1");
-                        w3.setText("1");
+                        w1.setText("40kg-50kg");
+                        w2.setText("51kg-60kg");
+                        w3.setText("61kg-70kg");
+                        h1.setText("150cm-160cm");
+                        h2.setText("161cm-170cm");
+                        h3.setText("171cm-180cm");
                         break;
                 }
 
@@ -102,4 +105,11 @@ public class SignUpActivity extends AppCompatActivity{
         spinYear.setAdapter(adapter);
     }
 
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(SignUpActivity.this,"Saved",Toast.LENGTH_SHORT).show();
+        Intent intent = null;
+        intent = new Intent(SignUpActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
 }
